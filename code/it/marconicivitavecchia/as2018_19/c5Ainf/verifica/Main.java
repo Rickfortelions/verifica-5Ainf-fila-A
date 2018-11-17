49099b3b1275a7dde2a8f2a21274426818958adc
Riccardo <riccardoforteleoni@hotmail.it>
2018-11-17 10:08:41 +0100

    Consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -12,16 +12,58 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(600,500);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb = new StringBuilder("html");
+
 		// TODO Creare la stringa HTML per il wireframe
-		
+		sb.append("<html>");
+		sb.append("<h1>SSH First Aid</h1>");
+		// Create div for table
+		sb.append("<div style='display:table'>");
+		// create the first row
+		sb.append("<div style='display:table-row'>");
+		// create the Product cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Alias");
+		sb.append("</div>");
+		// create the Price cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Hostname");
+		sb.append("</div>");
+		// close first row
+		sb.append("</div>");
+		sb.append("<div style='display:table-row'>");
+        // create the Product cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Raspberry");
+		sb.append("</div>");
+		// create the Price cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("server.marconi.it");
+		sb.append("</div>");
+		// close third row
+		sb.append("</div>");
+		// create the second row
+		sb.append("<div style='display:table-row'>");
+		// create the Product cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("CoreSwitch");
+		sb.append("</div>");
+		// create the Price cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("10.0.100.1");
+		sb.append("</div>");
+		// close third row
+		sb.append("</div>");
+		// close div for table
+		sb.append("</div>");
+		sb.append("</html>");
 		// TODO Convertire lo StringBuilder in String
-		
+		String str = sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(str));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
